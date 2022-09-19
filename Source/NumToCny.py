3beabb0a57328b05e8a71df345fe1cf3e1df1b69
Dvel <git@dvel.me>
2022-09-19 14:26:42 +0800

    适配 Python 3

--- Source/NumToCny.py
+++ Source/NumToCny.py
@@ -1,4 +1,4 @@
-#encoding: utf-8
+# encoding: utf-8
 '''
 中文互联网上迄今为止实现最正确、代码最漂亮且效率最高的大写人民币金额转换代码
 作者：李维 <oldrev@gmail.com>
@@ -6,12 +6,13 @@
 本代码基于 BSD License 授权。
 '''
 
-from cStringIO import StringIO
+from io import StringIO
 from decimal import Decimal
 import math
 
-_RMB_DIGITS = ['零', '壹', '贰', '叁', '肆', '伍', '陆', '柒', '捌', '玖' ]
-_SECTION_CHARS = ['', '拾', '佰', '仟', '万' ]
+_RMB_DIGITS = ['零', '壹', '贰', '叁', '肆', '伍', '陆', '柒', '捌', '玖']
+_SECTION_CHARS = ['', '拾', '佰', '仟', '万']
+
 
 def to_rmb_upper(price):
     price = Decimal(price)
@@ -23,11 +24,10 @@ def to_rmb_upper(price):
     qian_part = integer_part % 10000
     dec_part = int(price * 100 % 100)
 
-
     strio = StringIO()
 
     zero_count = 0
-    #处理万亿以上的部分
+    # 处理万亿以上的部分
     if integer_part >= 1000000000000 and wanyi_part > 0:
         zero_count = _parse_integer(strio, wanyi_part, zero_count, True)
         if yi_part > 0:
@@ -35,19 +35,19 @@ def to_rmb_upper(price):
         else:
             strio.write('万亿')
 
-    #处理亿到千亿的部分
+    # 处理亿到千亿的部分
     if integer_part >= 100000000 and yi_part > 0:
         is_first_section = integer_part >= 100000000 and integer_part < 1000000000000
         zero_count = _parse_integer(strio, yi_part, zero_count, is_first_section)
         strio.write('亿')
 
-    #处理万的部分
+    # 处理万的部分
     if integer_part >= 10000 and wan_part > 0:
         is_first_section = integer_part >= 1000 and integer_part < 10000000
         zero_count = _parse_integer(strio, wan_part, zero_count, is_first_section)
         strio.write('万')
 
-    #处理千及以后的部分
+    # 处理千及以后的部分
     if qian_part > 0:
         is_first_section = integer_part < 1000
         zero_count = _parse_integer(strio, qian_part, zero_count, is_first_section)
@@ -56,7 +56,7 @@ def to_rmb_upper(price):
     if integer_part > 0:
         strio.write('圆')
 
-    #处理小数
+    # 处理小数
     if dec_part > 0:
         _parse_decimal(strio, integer_part, dec_part, zero_count)
     elif dec_part == 0 and integer_part > 0:
@@ -66,12 +66,13 @@ def to_rmb_upper(price):
 
     return strio.getvalue()
 
-def _parse_integer(strio, value, zero_count = 0, is_first_section = False):
+
+def _parse_integer(strio, value, zero_count=0, is_first_section=False):
     assert value > 0 and value <= 9999
     ndigits = int(math.floor(math.log10(value))) + 1
     if value < 1000 and not is_first_section:
         zero_count += 1
-    for i in xrange(0, ndigits):
+    for i in range(0, ndigits):
         factor = int(pow(10, ndigits - 1 - i))
         digit = int(value / factor)
         if digit != 0:
@@ -82,12 +83,13 @@ def _parse_integer(strio, value, zero_count = 0, is_first_section = False):
             zero_count = 0
         else:
             zero_count += 1
-        value -= value / factor * factor
+        value -= value // factor * factor
     return zero_count
+
 
 def _parse_decimal(strio, integer_part, value, zero_count):
     assert value > 0 and value <= 99
-    jiao = value / 10
+    jiao = value // 10
     fen = value % 10
     if zero_count > 0 and (jiao > 0 or fen > 0) and integer_part > 0:
         strio.write('零')
